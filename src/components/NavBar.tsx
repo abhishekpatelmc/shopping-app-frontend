54ace3524190ac7d445eccb102f32b1f915a0e2d
Abhishek patel <mcabhishekpatel2017@gmail.com>
2024-04-17 02:18:45 -0400

    Update tailwind.config.ts, src/components/NavBar.tsx, src/app/globals.css, package.json, src/app/layout.tsx, package-lock.json, and public/next.svg

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import Avatar from "boring-avatars";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex justify-between border-b border-secondary px-4 py-4 items-center text-primary">
+        <div>
+          <Image width={48} height={48} src="/navLogo.png" alt="Navbar Logo" />
+        </div>
+        <div>
+          <Avatar
+            size={38}
+            name="Maggie"
+            variant="beam"
+            colors={["#CCCC66", "#A6BF73", "#80B380", "#59A68C", "#339999"]}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#f1f1f1",
+        background: "#202124",
+        secondary: "#CCCC66",
       },
     },
   },
